21814d93e09edf102b1f81105defedcf00269411
robert wilson <hotaru@thinkindifferent.net>
2010-11-30 23:31:02 -0500

    enable swap, change compiler path

--- make-kernel.sh
+++ make-kernel.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-CCOMPILER=~/android-cm/prebuilt/linux-x86/toolchain/arm-eabi-4.4.0/bin/arm-eabi-
-export CCOMPILER=~/android-cm/prebuilt/linux-x86/toolchain/arm-eabi-4.4.0/bin/arm-eabi-
+CCOMPILER=~/android/system/prebuilt/linux-x86/toolchain/arm-eabi-4.4.0/bin/arm-eabi-
+export CCOMPILER=~/android/system/prebuilt/linux-x86/toolchain/arm-eabi-4.4.0/bin/arm-eabi-
 make UncappedStable_defconfig
 make ARCH=arm CROSS_COMPILE=$CCOMPILER -j3
