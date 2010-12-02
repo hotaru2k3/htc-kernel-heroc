bfb36aec431f7085e8a9527848f769e2248bfd4c
Decad3nce <Decad3nce@tweaktool.com>
2010-12-02 17:46:59 -0600

    Better script by s0be

--- make-kernel.sh
+++ make-kernel.sh
@@ -1,5 +1,31 @@
 #!/bin/bash
+[ -z $ANDROID ] && ANDROID=/media/android
+
+me=`basename $0`
 CCOMPILER=~/android-cm/prebuilt/linux-x86/toolchain/arm-eabi-4.4.0/bin/arm-eabi-
-export CCOMPILER=~/android-cm/prebuilt/linux-x86/toolchain/arm-eabi-4.4.0/bin/arm-eabi-
-make UncappedStable_defconfig
-make ARCH=arm CROSS_COMPILE=$CCOMPILER -j3
+export CCOMPILER
+
+echo "Using $CCOMPILER"
+
+if [[ "${1}" == "DC" ]]
+then
+        make ARCH=arm CROSS_COMPILE=$CCOMPILER UncappedStable_defconfig
+else
+        if [[ "${1}" == "MC" ]]
+        then
+                make ARCH=arm CROSS_COMPILE=$CCOMPILER menuconfig
+        else
+                if [[ "${1}" == "K" ]]
+                then
+                        make ARCH=arm CROSS_COMPILE=$CCOMPILER ${2}
+                else
+                        echo "Usage: "
+                        echo " ANDROID=/path/to/android/ ${me} <action>"
+                        echo
+                        echo " Supported Actions: "
+                        echo "  MC     -     Menuconfig "
+                        echo "  DC     -     Default Config "
+                        echo "  K      -     Kernel"
+                fi
+        fi
+fi
